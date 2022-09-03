d534aa264c0a21f1e2cfc8cc2f5968bb7d448d91
DmitriySolenkov <soledimk@gmail.com>
2022-09-03 14:09:51 +0300

    Ex54

---
+++ Ex54/Program.cs
@@ -0,0 +1,67 @@
+﻿int[,] FillMatrix(int length1, int length2)
+{
+    int[,] array = new int[length1, length2];
+    for (int i = 0; i < length1; i++)
+    {
+        for (int j = 0; j < length2; j++)
+        {
+            array[i, j] = new Random().Next(0, 11);
+        }
+    }
+    return array;
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int res = Convert.ToInt32(Console.ReadLine());
+    return res;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int k = 0; k < matrix.GetLength(0); k++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            Console.Write($"{matrix[k, l]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int line = Prompt("Введите число строк: ");
+int column = Prompt("Введите число колонок: ");
+if (line > 0 && column > 0)
+{
+    int[,] matrix = FillMatrix(line, column);
+    PrintMatrix(matrix);
+    Console.WriteLine();
+
+    for (int k = 0; k < matrix.GetLength(0); k++)
+    {
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            int max = matrix[k, i];
+            int maxInd = i;
+            for (int j = i; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[k, j] > max)
+                {
+                    max = matrix[k, j];
+                    maxInd = j;
+                }
+            }
+            int buffer = matrix[k, i];
+            matrix[k, i] = matrix[k, maxInd];
+            matrix[k, maxInd] = buffer;
+        }
+    }
+
+    PrintMatrix(matrix);
+
+}
+else
+{
+    Console.WriteLine("Введите корректное число строк и столбцов!");
+}
